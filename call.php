38b494455938ed6c96ab5e4aec1ae06c322b45fc
manojdhiman <manoj@rudrainnovatives.com>
2014-09-13 10:16:24 +0530

    Create call.php

---
+++ call.php
@@ -0,0 +1,18 @@
+$total=wp_count_posts('press_release');
+				$pager_id = (get_query_var('paged')) ? get_query_var('paged') : 1;
+				$my_query = new WP_Query('post_type=press_release&post_status=publish&posts_per_page=9&meta_key=date_release&orderby=date_release&order=DESC&&paged='.$pager_id);
+				if( $my_query->have_posts() ) {
+				  while ($my_query->have_posts()) : $my_query->the_post(); 
+					
+					
+					//code here
+             
+				  endwhile;
+				}
+				wp_reset_query();  // Restore global post data stomped by the_post().
+			?>                          
+				 
+				 
+				 // pagination div
+				 
+				 <?php appcara_pagination(ceil($total->publish/9)); ?>
